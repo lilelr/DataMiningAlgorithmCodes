af2566b8494ec65240a574a1e5bf6ff3bd8c4727
lilelr <12211104lilele>
2017-04-04 16:23:13 +0800

    Initial Commit

---
+++ DataMingingAlgorithmCodes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataMingingAlgorithmCodes
+//
+//  Created by yuxiaoMac on 4/4/17.
+//  Copyright © 2017 yuxiaoMac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
